13d24a9407aaa0c8eb969874852cd87d2741a5d0
Piotrek <Piotrek@PIOTREK>
2017-11-26 10:56:14 +0100

    Create a skeleton of the solution

---
+++ MyStore/MyStore/MyStore.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyStore.UI.Startup))]
+namespace MyStore.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
